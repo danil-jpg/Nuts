02b5f6ee5c1fc50d790f5aa227ae8e4bcd00cfae
Danil <danil.polovinkin.1994@gmail.com>
2022-09-20 16:58:47 +0300

    fixed video problem

--- src/scripts/cardsSlider.js
+++ src/scripts/cardsSlider.js
@@ -21,6 +21,18 @@ function cardSlider() {
       modules: [Navigation, Pagination],
     });
   });
+
+  let swiper = new Swiper(`.swiper-video`, {
+    slidesPerView: 1,
+    centeredSlides: false,
+    loop: true,
+    direction: "horizontal",
+    navigation: {
+      nextEl: ".swiper-button-vid-next",
+      prevEl: ".swiper-button-vid-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
 }
 
 export default cardSlider;

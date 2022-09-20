ddf3c955056895e126659d54a8b2d2974f58035d
Danil <danil.polovinkin.1994@gmail.com>
2022-09-20 13:57:53 +0300

    fixed another bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,39 +5,23 @@ import burger from "./burger.js";
 window.addEventListener("DOMContentLoaded", () => {
   burger();
 
-  const swiper = new Swiper(".swiper-production", {
-    slidesPerView: 1,
-    centeredSlides: false,
-    loop: true,
-    direction: "horizontal",
-    // autoHeight: "true",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    modules: [Navigation, Pagination],
-  });
-
   const prodSliders = document.querySelectorAll(
-      ".production__card .swiper-production"
-    ),
-    prodSlidersNext = document.querySelectorAll(".swiper-button-next"),
-    prodSlidersPrev = document.querySelectorAll(".swiper-button-prev");
+    ".production__card .swiper-production"
+  );
 
-  // prodSliders.forEach((item, index) => {
-  //   item.classList.add(`swiper-production_${index}`);
-  //   console.log(item);
-  //   let swiper = new Swiper(`.swiper-production_${index}`, {
-  //     slidesPerView: 1,
-  //     centeredSlides: false,
-  //     loop: true,
-  //     direction: "horizontal",
-  //     // autoHeight: "true",
-  //     navigation: {
-  //       nextEl: ".swiper-button-next",
-  //       prevEl: ".swiper-button-prev",
-  //     },
-  //     modules: [Navigation, Pagination],
-  //   });
-  // });
+  prodSliders.forEach((item, index) => {
+    item.classList.add(`swiper-production_${index}`);
+
+    let swiper = new Swiper(`.swiper-production_${index}`, {
+      slidesPerView: 1,
+      centeredSlides: false,
+      loop: true,
+      direction: "horizontal",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      modules: [Navigation, Pagination],
+    });
+  });
 });

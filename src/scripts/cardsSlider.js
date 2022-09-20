f6278e3f30fb1d1619da4853641fcbd68580599c
Danil <danil.polovinkin.1994@gmail.com>
2022-09-20 16:13:43 +0300

    started next section mainPage

---
+++ src/scripts/cardsSlider.js
@@ -0,0 +1,26 @@
+import slider from "./_swiper.js";
+import Swiper, { Navigation, Pagination } from "swiper";
+
+function cardSlider() {
+  const prodSliders = document.querySelectorAll(
+    ".production__card .swiper-production"
+  );
+
+  prodSliders.forEach((item, index) => {
+    item.classList.add(`swiper-production_${index}`);
+
+    let swiper = new Swiper(`.swiper-production_${index}`, {
+      slidesPerView: 1,
+      centeredSlides: false,
+      loop: true,
+      direction: "horizontal",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      modules: [Navigation, Pagination],
+    });
+  });
+}
+
+export default cardSlider;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,14 @@
 import slider from "./_swiper.js";
 import Swiper, { Navigation, Pagination } from "swiper";
 import burger from "./burger.js";
+import cardSlider from "./cardsSlider.js";
 
 window.addEventListener("DOMContentLoaded", () => {
+  const mainPage = document.querySelector(".mainPage");
+
   burger();
 
-  const prodSliders = document.querySelectorAll(
-    ".production__card .swiper-production"
-  );
-
-  prodSliders.forEach((item, index) => {
-    item.classList.add(`swiper-production_${index}`);
-
-    let swiper = new Swiper(`.swiper-production_${index}`, {
-      slidesPerView: 1,
-      centeredSlides: false,
-      loop: true,
-      direction: "horizontal",
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      modules: [Navigation, Pagination],
-    });
-  });
+  if (mainPage) {
+    cardSlider();
+  }
 });

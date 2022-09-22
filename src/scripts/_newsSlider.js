50686f18b45122c4aaa1b407ce9d43dfc6668e5d
Danil <danil.polovinkin.1994@gmail.com>
2022-09-22 13:29:44 +0300

    adaptated adaptation to  component

--- src/scripts/_newsSlider.js
+++ src/scripts/_newsSlider.js
@@ -13,6 +13,21 @@ function newsSlider() {
       prevEl: ".swiper-news-prev",
     },
     modules: [Navigation, Pagination],
+    breakpoints: {
+      1440: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 1.5,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 1,
+      },
+    },
   });
 }
 

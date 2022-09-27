e5912daea3aff8546be714d233a10873bfeb1621
Danil <danil.polovinkin.1994@gmail.com>
2022-09-27 17:19:40 +0300

    completed page newsAndArticles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,9 +8,8 @@ import videoSlider from "./_videoSlider.js";
 window.addEventListener("DOMContentLoaded", () => {
   const mainPage = document.querySelector(".mainPage"),
     shopPage = document.querySelector(".shopPage"),
-    aboutPage = document.querySelector(".about");
-
-  burger();
+    aboutPage = document.querySelector(".about"),
+    newsAndArticlesPage = document.querySelector(".newsAndArticlesPage");
 
   if (mainPage) {
     cardSlider();
@@ -26,4 +25,35 @@ window.addEventListener("DOMContentLoaded", () => {
     newsSlider();
     videoSlider();
   }
+  if (newsAndArticlesPage) {
+    let swiper = new Swiper(`.swiper-news`, {
+      spaceBetween: 32,
+      slidesPerView: 3,
+      centeredSlides: false,
+      loop: true,
+      direction: "horizontal",
+      navigation: {
+        nextEl: ".swiper-news-next",
+        prevEl: ".swiper-news-prev",
+      },
+      modules: [Navigation, Pagination],
+      breakpoints: {
+        1440: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 1.5,
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 1,
+        },
+      },
+    });
+  }
+
+  burger();
 });
